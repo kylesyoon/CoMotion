7e729d5287f326a1fbac6db2b182400fc2d6d07e
Kyle Yoon <kyle.yoon@grainger.com>
2018-07-02 20:04:46 -0500

    Initial Commit

---
+++ CoMotionTests/CoMotionTests.swift
@@ -0,0 +1,36 @@
+//
+//  CoMotionTests.swift
+//  CoMotionTests
+//
+//  Created by Yoon, Kyle on 7/2/18.
+//  Copyright © 2018 Yoon, Kyle. All rights reserved.
+//
+
+import XCTest
+@testable import CoMotion
+
+class CoMotionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
