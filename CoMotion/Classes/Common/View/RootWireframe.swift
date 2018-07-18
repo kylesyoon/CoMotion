288f7272798b54daf16e101ef53b73721214efbd
Kyle Yoon <kyle.yoon@grainger.com>
2018-07-18 11:20:40 -0500

    Root navigation controller to show first view controller

---
+++ CoMotion/Classes/Common/View/RootWireframe.swift
@@ -0,0 +1,28 @@
+//
+//  RootWireframe.swift
+//  CoMotion
+//
+//  Created by Yoon, Kyle on 7/18/18.
+//  Copyright © 2018 Yoon, Kyle. All rights reserved.
+//
+
+import UIKit
+
+class RootWireframe {
+    func showRootViewController(_ viewController: UIViewController, in window: UIWindow) {
+        guard let navigationController = rootNavigationController(in: window) else {
+            return
+        }
+        
+        navigationController.viewControllers = [viewController]
+    }
+    
+    func rootNavigationController(in window: UIWindow) -> UINavigationController? {
+        guard let rootNavigationController = window.rootViewController as? UINavigationController else {
+            assertionFailure("Couldn't get root navigation controller from window.")
+            return nil
+        }
+        
+        return rootNavigationController
+    }
+}
