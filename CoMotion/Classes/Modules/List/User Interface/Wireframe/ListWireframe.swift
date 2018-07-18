8666b4e795ea83cca561d8bbb191e97a1d219948
Kyle Yoon <kyle.yoon@grainger.com>
2018-07-18 11:25:34 -0500

    Present ListViewController in root

--- CoMotion/Classes/Modules/List/User Interface/Wireframe/ListWireframe.swift
+++ CoMotion/Classes/Modules/List/User Interface/Wireframe/ListWireframe.swift
@@ -9,15 +9,27 @@
 import UIKit
 
 class ListWireframe {
+    var rootWireframe: RootWireframe?
+    
     var mainStoryboard: UIStoryboard {
         return UIStoryboard(name: "Main", bundle: nil)
     }
     
-    var listViewControllerFromStoryboard: UIViewController {
-        return self.mainStoryboard.instantiateViewController(withIdentifier: String(describing: ListViewController.self))
+    var listViewController: ListViewController?
+    var listViewControllerFromStoryboard: ListViewController? {
+        guard let listViewController = self.mainStoryboard.instantiateViewController(withIdentifier: String(describing: ListViewController.self)) as? ListViewController else {
+            assertionFailure("Couldn't get ListViewController from Main storyboard.")
+            return nil
+        }
+        
+        return listViewController
     }
     
     func presentListInterface(from window: UIWindow) {
-
+        guard let rootWireframe = rootWireframe, let listViewController = listViewControllerFromStoryboard else {
+            return
+        }
+        
+        rootWireframe.showRootViewController(listViewController, in: window)
     }
 }
