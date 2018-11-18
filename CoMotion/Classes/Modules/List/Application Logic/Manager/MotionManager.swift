acc5f6a68ddfe0504d5b905a35886e20d93819c2
Kyle Sejin Yoon <apps.yoon@gmail.com>
2018-11-18 11:00:00 -0800

    Added mock device motion for simulators

--- CoMotion/Classes/Modules/List/Application Logic/Manager/MotionManager.swift
+++ CoMotion/Classes/Modules/List/Application Logic/Manager/MotionManager.swift
@@ -27,8 +27,13 @@ class MotionManager {
         
         return queue
     }()
-
+    
+    private var mockTimer: Timer?
+    
     func start() {
+        #if targetEnvironment(simulator)
+        startMock()
+        #else
         guard motionManager.isDeviceMotionAvailable else {
             print("Device Motion is not available.")
             return
@@ -39,15 +44,56 @@ class MotionManager {
             print(deviceMotion)
             strongSelf.delegate?.motionManager(strongSelf, didUpdate: deviceMotion.com_motionValue)
         }
+        #endif
     }
     
     func stop() {
+        #if targetEnvironment(simulator)
+        stopMock()
+        #else
         guard motionManager.isDeviceMotionAvailable else { return }
         motionManager.stopDeviceMotionUpdates()
+        #endif
     }
     
     deinit {
         stop()
     }
     
+    private func startMock() {
+        let now = Date()
+        mockTimer = Timer.scheduledTimer(withTimeInterval: motionManager.deviceMotionUpdateInterval,
+                                         repeats: true) { timer in
+                                            let timeInterval = timer.fireDate.timeIntervalSince(now)
+                                            let sinSignal = sin(timeInterval)
+                                            let attitude = Attitude(roll: sinSignal,
+                                                                    pitch: sinSignal,
+                                                                    yaw: sinSignal)
+                                            let rotRate = Coordinates3D(x: sinSignal,
+                                                                        y: sinSignal,
+                                                                        z: sinSignal)
+                                            let gravity = Coordinates3D(x: sinSignal,
+                                                                        y: sinSignal,
+                                                                        z: sinSignal)
+                                            let userAcc = Coordinates3D(x: sinSignal,
+                                                                        y: sinSignal,
+                                                                        z: sinSignal)
+                                            let motion = Motion(attitude: attitude,
+                                                                rotationRate: rotRate,
+                                                                gravity: gravity,
+                                                                userAcceleration: userAcc,
+                                                                heading: sinSignal,
+                                                                timestamp: timeInterval)
+                                            self.delegate?.motionManager(self, didUpdate: motion)
+        }
+        mockTimer?.fire()
+    }
+    
+    private func stopMock() {
+        mockTimer?.invalidate()
+        mockTimer = nil
+    }
+    
 }
+
+
